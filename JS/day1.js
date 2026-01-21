616b1d445d67a70cdedf3df2813827486a7c2b24
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-21 11:40:36 +0000

    "Day 1: Variables and data types"

---
+++ JS/day1.js
@@ -0,0 +1,9 @@
+let myName = "Madan.R";
+let myAge = "21";
+let isStudent = "true";
+
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("student:", isStudent);
+
+console.log(myName, myAge, isStudent);
