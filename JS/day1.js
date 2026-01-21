31bbe769052e16b5521b4a3da5a220f3f3c7d048
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-21 12:45:18 +0000

    "Day 1: Variables and Data Types (10 programs)"

--- JS/day1.js
+++ JS/day1.js
@@ -1,4 +1,4 @@
-/*
+
 let myName = "Madan.R";
 let myAge = "21";
 let isStudent = "true";
@@ -8,26 +8,29 @@ console.log("Text:", text);
 console.log("Name:", myName);
 console.log("Age:", myAge);
 console.log("student:", isStudent);
-
 console.log(text, myName, myAge, isStudent);
-*/
 
-/*
-let myName = "Madan";
-console.log(myName);
 
+// 1 store and print name
+let Name = "Madan";
+console.log(Name);
+
+// 2 store Age
 let age = 21;
 console.log(age);
 
+// 3 boolean value
 let isDeveloper = true;
 console.log(isDeveloper);
 
+// 4 multiple variables together
 let city = "Mysuru";
 let pincode = 571129
 
 console.log(city);
 console.log(pincode);
-*/
+
+// 5 check data type
 
 let score = 95;
 let username = "coder";
@@ -36,3 +39,31 @@ let passed = "true";
 console.log(typeof score);
 console.log(typeof username);
 console.log(typeof passed);
+
+// 6 null & undefined
+let job = null;
+let salary;
+
+console.log(job);
+console.log(salary);
+
+
+// 7 change variable value
+
+let price = 100;
+
+price = 150;
+console.log(price);
+
+let newscore = 10;
+
+newscore = 20;
+newscore = newscore + 5;
+console.log(newscore);
+
+
+// 8 constant veriables 
+const country = "india";
+console.log(country);
+
+console.log(text, myName, myAge, isStudent, isDeveloper, city, pincode, score, username, passed, job, salary, price, newscore, country);
