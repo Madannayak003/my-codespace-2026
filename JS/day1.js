06b9b119570a3396f03e0943d4feccd3aec15262
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-21 12:11:06 +0000

    "multiple variables together"

--- JS/day1.js
+++ JS/day1.js
@@ -1,9 +1,29 @@
+/*
 let myName = "Madan.R";
 let myAge = "21";
 let isStudent = "true";
+let text = "Hello";
 
+console.log("Text:", text);
 console.log("Name:", myName);
 console.log("Age:", myAge);
 console.log("student:", isStudent);
 
-console.log(myName, myAge, isStudent);
+console.log(text, myName, myAge, isStudent);
+*/
+
+let myName = "Madan";
+console.log(myName);
+
+let age = 21;
+console.log(age);
+
+let isDeveloper = true;
+console.log(isDeveloper);
+
+let city = "Mysuru";
+let pincode = 571129
+
+console.log(city);
+console.log(pincode);
+
