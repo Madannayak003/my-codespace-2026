916cfd17f5c547647f88eef9dcacdd78912acf50
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-22 13:02:37 +0000

    Check Positive/Negative and Even/Odd

---
+++ JS/day2.js
@@ -0,0 +1,21 @@
+// 1 Check Positive or Negative
+let num = 10;
+
+if (num > 0){
+    console.log("positive number");
+} else {
+    console.log("negative number");
+}
+
+// 2 Even or Odd
+
+let number = 10;
+
+if (number %  2 === 0){
+    console.log("Even");
+} else {
+    console.log("odd")
+}
+
+
+
