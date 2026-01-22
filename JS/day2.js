0c213e503649c22c76897b4f9736942194de1a9b
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-22 13:04:03 +0000

    Add voting eligibility, largest number comparisons, and leap year check

--- JS/day2.js
+++ JS/day2.js
@@ -17,5 +17,51 @@ if (number %  2 === 0){
     console.log("odd")
 }
 
+// 3 Voting Eligibility
+
+let age = 22;
+
+if (age >= 18){
+    console.log("You are eligible to vote");
+} else {
+    console.log("You are not eligibile to vote")
+}
+
+// 4 largest of two number
+
+let a = 5;
+let b = 20;
+
+if (a > b){
+    console.log("a is greater");
+} else {
+    console.log("b is greater");    
+
+}
+
+// 5 largest of three number
+
+let x = 10;
+let y = 25;
+let z = 15;
+
+if (x > y && X > Z) {
+    console.log("x is greatest");
+} else if (y > x && y > z){
+    console.log("y is greatest");
+} else {
+    console.log("z is greatest");
+}
+
+// 6 Check Leap Year
+
+let year = 2026;
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+}
+
 
 
