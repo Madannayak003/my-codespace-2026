13a290f5677c85e8fa6921f1e3c877bd0fd0a066
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-22 13:41:57 +0000

    Refine output messages and add additional checks for pass/fail, grade calculation, equality, and login functionality

--- JS/day2.js
+++ JS/day2.js
@@ -12,9 +12,9 @@ if (num > 0){
 let number = 10;
 
 if (number %  2 === 0){
-    console.log("Even");
+    console.log("Even number");
 } else {
-    console.log("odd")
+    console.log("odd number")
 }
 
 // 3 Voting Eligibility
@@ -63,5 +63,50 @@ if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
     console.log("Not a Leap Year");
 }
 
+// 7 pass or fail
 
+let score = 45;
 
+if (score >= 35){
+    console.log("pass");
+} else {
+    console.log("fail");
+}
+
+// 8 Grade Calculation
+
+let marks = 91.5;
+
+if (marks >= 90){
+    console.log("Grade A");
+} else if (marks >= 80){
+    console.log("Grade B");
+} else if (marks >= 70){
+    console.log("Grade C");
+} else if (marks >= 60){
+    console.log("Grade D");
+} else {
+    console.log("Grade F");
+}
+
+// 9 check equality
+
+let k = 10;
+let l = 10;
+
+if (k === l){
+    console.log("Both are Equal");
+} else {
+    console.log("Not Equal");
+}
+
+// Login check
+
+let username = "admin";
+let password = "12345";
+
+if (username === "admin" && password === "12345"){
+    console.log("LOGIN SUCCESSFUL");
+} else {
+    console.log("LOGIN FAILED");
+}
