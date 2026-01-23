85287b3dab0b872a3b66b7d70129e809f4c39d73
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-23 14:33:45 +0000

    Add additional number printing examples and sum calculation

--- JS/day3.js
+++ JS/day3.js
@@ -1,3 +1,37 @@
+// 1: Print Numbers 1 to 5 (for loop)
+
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
+
+// 2: Print 1 to 10
+
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// 3: Print Even Numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+// 4: Print Odd Numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+// 5: Sum of Numbers (1–5)
+
+let sum = 0;
+
+for (let i =1; i <=5; i++){
+    sum = sum  + i;
+}
+
+console.log("sum:",sum);
