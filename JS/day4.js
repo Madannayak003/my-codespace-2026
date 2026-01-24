3566dd325afe4822311e0d3ed5fdad9fcce90ce7
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-24 13:51:21 +0000

    Add simple greeting functions in Day 4 JavaScript file

---
+++ JS/day4.js
@@ -0,0 +1,14 @@
+// 1: Simple Function
+
+function greet(){
+    console.log("Hello World!");
+}
+greet(); // Output: Hello World!
+
+// 2: Function with Parameters
+
+function greetuser(name){
+    console.log("Hello", name);
+}
+
+greetuser("Madan.R!"); // Output: Hello Madan.R!
