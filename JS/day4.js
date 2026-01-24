f3c060f5809c18b2d4403774cf14e3c8421059c2
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-24 13:59:25 +0000

    Add addition and square functions to Day 4 JavaScript file

--- JS/day4.js
+++ JS/day4.js
@@ -12,3 +12,20 @@ function greetuser(name){
 }
 
 greetuser("Madan.R!"); // Output: Hello Madan.R!
+
+// 3: add two Numbers
+
+function add(a, b){
+    console.log(a + b);
+}
+
+add(5, 10); // Output: 15
+
+// 4: Function with Return Value
+
+function square(num){
+    return num * num;   
+}
+
+let result = square(4);
+console.log(result); // Output: 16
