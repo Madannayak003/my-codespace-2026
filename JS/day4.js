42f8e7b9ddd8fb8c74521727f079a0ce85a95e4a
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-24 14:07:33 +0000

    Add even/odd and age check functions to Day 4 JavaScript file

--- JS/day4.js
+++ JS/day4.js
@@ -29,3 +29,29 @@ function square(num){
 
 let result = square(4);
 console.log(result); // Output: 16
+
+// 5: Even or Odd Function
+
+function isEven(num){
+    if ( num % 2 === 0 ){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(isEven(7)); // Output: Odd
+console.log(isEven(10)); // Output: Even
+
+// 6: Function With condition
+
+function checkAge(age){
+    if(age >= 18){
+        return "Eligible to vote";
+    } else {
+        return "Not Eligible to vote";
+    }
+}
+
+console.log(checkAge(20)); // Output: Eligible to vote
+console.log(checkAge(16)); // Output: Not Eligible to vote
