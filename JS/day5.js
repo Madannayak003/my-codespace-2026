7ed4dcb57e8fed1d45e1469054f5007814d8f31b
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-25 06:01:47 +0000

    Add initial implementation of fruit array with access and modification examples

---
+++ JS/day5.js
@@ -0,0 +1,21 @@
+// 1: create and print an Array.
+ 
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits);
+
+// 2: Accessing elements by index.
+
+console.log(fruits[0]); // First element
+console.log(fruits[1]); // Second element
+console.log(fruits[2]); // Third element    
+
+// 3: Array Length.
+
+console.log(fruits.length); // Output: 3
+
+// 4: change Array Value.
+
+fruits[1] = "orange"; // Change "Banana" to "orange"
+
+console.log(fruits); // Output: ["Apple", "orange", "Mango"]
