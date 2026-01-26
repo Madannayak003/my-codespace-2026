106b25870f74c1fec95e8770a2719cd8f03d26d3
Madan R <130026806+Madannayak003@users.noreply.github.com>
2026-01-26 16:25:29 +0000

    Add object creation and value access examples in Day 6 JavaScript file

---
+++ JS/day6.js
@@ -0,0 +1,21 @@
+// 1: create & print object.
+
+let person = {
+    name: "MAdan.R",
+    age: 22,
+    city: "Bangalore"
+};
+
+console.log(person);
+
+// 2: Access Object Values (Dot notation)
+
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+console.log("City:", person.city);
+
+// 3: Access Object Values (Bracket notation)
+
+console.log("Name:", person["name"]);
+console.log("Age:", person["age"]);
+console.log("City:", person["city"]);
